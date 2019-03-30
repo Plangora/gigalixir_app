f271fab8acc9227eb2347827d3053a6d3c417182
Allen Wyma <allen@plangora.com>
2019-03-29 23:41:10 -0700

    update greeting

--- lib/gigalixir_phoenix/timer.ex
+++ lib/gigalixir_phoenix/timer.ex
@@ -1,7 +1,7 @@
 defmodule GigalixirPhoenix.Timer do
     use GenServer
     alias GigalixirPhoenixWeb.Endpoint
-    @greeting "Hello"
+    @greeting "Hey!"
 
     def start_link(room), do: GenServer.start_link(__MODULE__, {room, 0, @greeting})
 
@@ -15,5 +15,9 @@ defmodule GigalixirPhoenix.Timer do
         Process.send_after(self(), :counter, 1000)
         Endpoint.broadcast(room, "count", %{timer: timer, greeting: greeting})
         {:noreply, {room, timer, greeting}}
+    end
+
+    def code_change(_old_vsn, {room, timer, _greeting}, _extra) do
+        {:ok, {room, timer, @greeting}}
     end
 end
