42e7c6441f33d536278f2312d8262848d579f06e
Allen Wyma <allen.wyma@gmail.com>
2019-04-01 15:34:53 +0800

    cleanup code for deployment for hot code upgrade

--- config/prod.exs
+++ config/prod.exs
@@ -11,8 +11,8 @@ use Mix.Config
 # before starting your production server.
 config :gigalixir_phoenix, GigalixirPhoenixWeb.Endpoint,
   http: [:inet6, port: {:system, "PORT"}],
-  # url: [host: "${APP_NAME}.gigalixirapp.com", port: 80],
-  url: [host: "localhost", port: 4000], # for testing prod locally
+  url: [host: "${APP_NAME}.gigalixirapp.com", port: 80],
+  # url: [host: "localhost", port: 4000], # for testing prod locally
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: "${SECRET_KEY_BASE}",
   server: true
--- lib/gigalixir_phoenix_web/channels/room_channel.ex
+++ lib/gigalixir_phoenix_web/channels/room_channel.ex
@@ -3,6 +3,6 @@ defmodule GigalixirPhoenixWeb.RoomChannel do
 
   def join(room, _params, socket) do
     {:ok, _pid} = DynamicSupervisor.start_child(GigalixirPhoenix.TimerSupervisor, {GigalixirPhoenix.Timer, room})
-    {:ok, assign(socket, :timer, 0)}
+    {:ok, socket, :timer, 0}
   end
 end
